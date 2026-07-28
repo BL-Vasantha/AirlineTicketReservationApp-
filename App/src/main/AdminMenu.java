f95da40901c3e8ff95753862e5a2c2a769d25f69
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-07-28 16:30:18 +0530

    [VASANTHA] added :UC1-Admin Menu

---
+++ App/src/main/AdminMenu.java
@@ -0,0 +1,33 @@
+package main;
+import util.ScannerHelper;
+public class AdminMenu {
+
+    public static void showMenu() {
+        boolean logout = false;
+        while (!logout) {
+            System.out.println("\n----------- ADMIN MENU -----------");
+            System.out.println("1. Manage Users");
+            System.out.println("2. Manage Flights");
+            System.out.println("3. View Reports");
+            System.out.println("4. Logout");
+            int choice = ScannerHelper.readInt("Enter your choice : ");
+            switch (choice) {
+                case 1:
+                    System.out.println("User Management feature will be implemented in future use cases.");
+                    break;
+                case 2:
+                    System.out.println("Flight Management feature will be implemented in future use cases.");
+                    break;
+                case 3:
+                    System.out.println("Reports feature will be implemented in future use cases.");
+                    break;
+                case 4:
+                    System.out.println("Logging out from Admin...");
+                    logout = true;
+                    break;
+                default:
+                    System.out.println("Invalid Choice.");
+            }
+        }
+    }
+}
