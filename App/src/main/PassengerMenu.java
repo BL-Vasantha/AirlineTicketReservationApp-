241e537f9614dcc3bc9988eae8f39f11283f5489
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-07-29 12:23:33 +0530

    [VASANTHA] added: UC2-Passenger Menu

--- App/src/main/PassengerMenu.java
+++ App/src/main/PassengerMenu.java
@@ -1,30 +1,51 @@
+
 package main;
+
+import model.Flight;
+import service.FlightService;
 import util.ScannerHelper;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class PassengerMenu {
 
     public static void showMenu() {
+
+        FlightService service = new FlightService();
         boolean logout = false;
+
         while (!logout) {
+
             System.out.println("\n--------- PASSENGER MENU ---------");
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.println("3. Manage Profile");
-            System.out.println("4. Logout");
+            System.out.println("1. Search Flights");
+            System.out.println("2. Logout");
+
             int choice = ScannerHelper.readInt("Enter your choice : ");
+
             switch (choice) {
+
                 case 1:
-                    System.out.println("Passenger Registration will be implemented in UC2.");
+                    System.out.print("Enter Source: ");
+                    String source = new java.util.Scanner(System.in).nextLine();
+
+                    System.out.print("Enter Destination: ");
+                    String dest = new java.util.Scanner(System.in).nextLine();
+
+                    List<Flight> flights =
+                            service.search(source, dest, LocalDate.now());
+
+                    if (flights.isEmpty()) {
+                        System.out.println("No flights found.");
+                    } else {
+                        flights.forEach(System.out::println);
+                    }
                     break;
+
                 case 2:
-                    System.out.println("Login feature will be implemented in future use cases.");
-                    break;
-                case 3:
-                    System.out.println("Profile Management will be implemented in future use cases.");
-                    break;
-                case 4:
-                    System.out.println("Logging out from Passenger...");
                     logout = true;
                     break;
+
                 default:
                     System.out.println("Invalid Choice.");
             }
